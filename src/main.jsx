8c2f2c79ea61d0478840c7bc9a8ad1a6e0a271db
SantiagoDelgado777 <zantysinx@gmail.com>
2026-05-29 06:41:07 -0300

    fix: import de style en singular

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles.css' // <--- Asegurate de que esta línea quede acá puesta
+import './style.css' // <--- Asegurate de que esta línea quede acá puesta
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
