6557ddfda237873f1e977b5a766e67fd00215b4f
SantiagoDelgado777 <zantysinx@gmail.com>
2026-05-29 06:19:27 -0300

    feat: dashboard de creadores con react y vite

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles.css' // <--- Asegurate de que esta línea quede acá puesta
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
